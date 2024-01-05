a0e4c3b39e80e197f5331812f0f7b64a72ef2daa
Sharon <sharonaliyas25@gmail.com>
2024-01-06 00:54:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'}
 
-# Change this with your url
+# Change this with your URL
 url = "https://github.com/SharonAliyas5573"
 
 # Get the page
@@ -25,7 +25,18 @@ stringday = today.strftime("%Y-%m-%d")
 # Get today's contributions
 result = soup.find("td", {"data-date": stringday})
 
-if result["data-level"] == "0":
-    sys.exit(os.EX_OK)
-else:
+if result["data-level"] != "0":
+    # Send push notification if contributions are not 0
+    push_notification_command = (
+        'echo "You have contributions today. Keep up the good work! 🚀" | '
+        'curl -X POST -H "Authorization: Bearer $PUSH_API_KEY" -H "Content-Type: application/json" '
+        '--data-binary @- https://api.pushbullet.com/v2/pushes'
+    )
+
+    os.system(push_notification_command)
+    
+    # Exit with an error status
     sys.exit(errno.ECANCELED)
+
+# Exit with a success status if contributions are 0
+sys.exit(os.EX_OK)
